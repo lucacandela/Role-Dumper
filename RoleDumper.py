887f8f4bbc73474271d25bcf6dad652267f9ffe6
Luca Candela <39223795+lucacandela@users.noreply.github.com>
2022-11-25 13:37:38 -0800

    Moved some code from RoleDumper to test file

--- RoleDumper.py
+++ RoleDumper.py
@@ -5,6 +5,3 @@ class RoleDumper:
     def createInviteLink():
         webLink = "https://discord.com/api/oauth2/authorize?client_id=" + DiscordTokens.client_ID + "&permissions=275146344512&scope=bot%20applications.commands"
         return webLink
-rd = RoleDumper
-
-print (rd.createInviteLink())
---
+++ test.py
@@ -0,0 +1,5 @@
+import RoleDumper
+
+rd = RoleDumper
+
+print (rd.createInviteLink())
