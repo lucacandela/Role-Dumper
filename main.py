de93069d362c062098829dc189cc0610095984f3
Luca Candela <39223795+lucacandela@users.noreply.github.com>
2022-11-25 17:13:10 -0800

    Added initial skeleton for slash commands

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import settings
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 logger = settings.logging.getLogger("bot")
@@ -13,6 +14,18 @@ def run():
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
         print("_______________")
+        # sync comands
+        try:
+            synced = await bot.tree.sync()
+            print(f"Synced {len(synced)} command")
+        except Exception as e:
+            print(e)
+
+
+    @bot.tree.command(name="list_members_with_role")
+    @app_commands.describe(role="Role")
+    async def listRoles(interaction: discord.Interaction, role: discord.Role):
+        await interaction.response.send_message(f"{interaction.user.nick} requested list of members with '{role}'")
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
